5a6fcda4a670d2e98ae2a6a838874523f1ae55e2
khalifsiregar123 <khalifsiregar123@gmail.com>
2025-05-12 01:34:44 +0700

    added file and fixing tailwind

---
+++ src/lib/components/locale-switcher.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { i18n } from '$lib/i18n';
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
+
+	function switchToLanguage(newLanguage) {
+		const canonicalPath = i18n.route($page.url.pathname);
+		const localisedPath = i18n.resolveRoute(canonicalPath, newLanguage);
+		goto(localisedPath);
+	}
+</script>
+
+<div>
+	<button on:click={() => switchToLanguage('en')}>en</button>
+	<button on:click={() => switchToLanguage('es')}>es</button>
+</div>
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { paraglide } from '@inlang/paraglide-sveltekit/vite';
+import svg from '@poppanator/sveltekit-svg';
+
+export default defineConfig({
+	plugins: [
+		sveltekit(),
+		paraglide({
+			project: './project.inlang',
+			outdir: './src/lib/paraglide'
+		}),
+		svg({
+			includePaths: ['./src/lib/icons/'],
+			svgoOptions: {
+				multipass: true,
+				plugins: [
+					{
+						name: 'preset-default',
+						params: { overrides: { removeViewBox: false } }
+					}
+				]
+			}
+		}),
+	],
+
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+});
